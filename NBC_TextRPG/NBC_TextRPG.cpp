f4800e77970cedc4a6dffc9b8339cd10c9784712
KHS2000 <115532803+KHS2000@users.noreply.github.com>
2026-05-22 10:18:41 +0900

    배틀시스템 기초 클레스

--- NBC_TextRPG/NBC_TextRPG.cpp
+++ NBC_TextRPG/NBC_TextRPG.cpp
@@ -9,9 +9,22 @@ class BattleSystem
 
 public:
 
-    BattleSystem(Player* &player,)
+    Player*& player;
+    Monster*& monster;
+
+    BattleSystem(Player* &player,Monster* &monster)
+        :player(player),monster(monster),
     {
 
+    }
+
+
+    void BattleStart()
+    {
+        if ((player->GetHp() > 0) && (monster->GetHP > 0))
+        {
+            cout << "전투시작!\n\n";
+        }
     }
 };
 
@@ -19,6 +32,8 @@ public:
 
 int main(int argc, char* argv[])
 {
+
+
 
     
     return 0;
