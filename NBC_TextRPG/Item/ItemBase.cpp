ebc79bf0faee8583ab4851441594c3e0e767b4e6
DESKTOP-JV9Q3R0\y8268 <y826855@naver.com>
2026-05-22 10:57:11 +0900

    feat: ItemManager 구성. ItemBase 정의

---
+++ NBC_TextRPG/Item/ItemBase.cpp
@@ -0,0 +1,17 @@
+#include "ItemBase.h"
+
+ItemBase::ItemBase(string _name, int _price)
+{
+    Name = _name;
+    Price = _price;
+}
+
+string ItemBase::GetName()
+{
+    return GetName();
+}
+
+int ItemBase::GetPrice()
+{
+    return Price;
+}
