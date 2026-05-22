c461198dad4a21baa012df28803eb77892190c95
mnns0805 <mnns0805@gmail.com>
2026-05-22 14:44:40 +0900

    몬스터 1차 수정 와꾸 설계
    몬스터 1차 수정 와꾸 설계

---
+++ NBC_TextRPG/Monster/Monster.cpp
@@ -0,0 +1,55 @@
+﻿#include "Monster.h"
+#include  "Player.h"
+
+Monster::Monster(string mName, int mHP, int mATK) 
+: mName(mName), mHP(mHP), mATK(mATK)
+{
+    SetStat(player);
+}
+
+Monster::~Monster()
+{
+    
+}
+
+
+void Monster::TakeDamage(int damage)
+{
+    
+}
+
+
+bool Monster::IsDeath()
+{
+    if (mHP <=0)
+    {
+        death = true;
+        cout << "몬스터 사망";
+    }
+    
+    else
+    {
+        death = false;
+    }
+    
+    return death;
+}
+
+
+
+int Monster::GetHP()
+{
+    return mHP;
+}
+
+
+
+void Monster::SetStat(Player* player)
+{
+    int randHP = rand() % 11 + 20;
+    int randATK = rand() % 6 + 5;
+    
+    mHP = player->GetLevel()*randHP; //함수 구현 예정
+    mATK = player->GetLevel()*randATK; //함수 구현 예정
+}
+
