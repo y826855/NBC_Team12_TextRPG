93130b350f58cf56bc363ced3a52d8580ce8a85e
KHS2000 <115532803+KHS2000@users.noreply.github.com>
2026-05-22 10:37:03 +0900

    배틀시스템 수정2

--- NBC_TextRPG/NBC_TextRPG.cpp
+++ NBC_TextRPG/NBC_TextRPG.cpp
@@ -28,21 +28,26 @@ public:
             player->PlayerAttack();
             cout << "플레이어가 공격합니다!";
 
+
             if ((player->GetHp() > 0) && (monster->GetHP > 0))
             {
                 cout << "전투승리!\n";
 
+                monster->GetDeath() = true;
+
                 delete monster;
                 monster = nullptr;
             }
-            else if ((player->GetHp() >= 0) && (monster->GetHP <= 0))
+            else if ((player->GetHp() <= 0) && (monster->GetHP > 0))
             {
                 cout << "전투 배패!\n 게임을 종료합니다.";
 
-                delete player;
-                player = nullptr;
+                player->GetDeath();
             }
-
+            else if ((player->GetHp() > 0) && (monster->GetHP > 0))
+            {
+                monster->GetDeath() = false;
+            }
         }
     }
 };
