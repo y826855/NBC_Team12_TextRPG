e56b185dc771b1f325086fe2bded2502a8e8615b
KHS2000 <115532803+KHS2000@users.noreply.github.com>
2026-05-26 08:57:01 +0900

    feat : 배틀시스템에 플레이어 공격, 몬스터 공격, 경험치와 골드 획득, 아이템 획득 함수 추가

--- NBC_TextRPG/BattleSystem.cpp
+++ NBC_TextRPG/BattleSystem.cpp
@@ -77,6 +77,7 @@ void BattleSystem::BattleStart()
             else if (player->IsDeath() && !monster->IsDeath())
             {
                 cout << "전투 배패!\n 게임을 종료합니다.";
+                return;
     
 
             }
@@ -98,6 +99,110 @@ void BattleSystem::BattleStart()
     }
 }
 
+
+
+void BattleSystem::PlayerAttack()
+{
+    int damage = player->GetATK();
+    monster->TakeDamage(damage);
+}
+
+
+void BattleSystem::MonsterAttack()
+{
+    int damage = monster->GetATK();
+    player->TakeDamage(damage);
+}
+
+
+void BattleSystem::BattleReward()
+{
+    
+}
+
+
+void BattleSystem::ItemReward()
+{
+    
+}
+
+
+void BattleSystem::PlayerAttack()
+{
+    int damage = player->GetATK();
+    monster->TakeDamage(damage);
+}
+
+
+void BattleSystem::MonsterAttack()
+{
+    int damage = monster->GetATK();
+    player->TakeDamage(damage);
+}
+
+
+void BattleSystem::BattleReward()
+{
+    
+}
+
+
+void BattleSystem::ItemReward()
+{
+    
+}
+
+
+void BattleSystem::PlayerAttack()
+{
+    int damage = player->GetATK();
+    monster->TakeDamage(damage);
+}
+
+
+void BattleSystem::MonsterAttack()
+{
+    int damage = monster->GetATK();
+    player->TakeDamage(damage);
+}
+
+
+void BattleSystem::BattleReward()
+{
+    
+}
+
+
+void BattleSystem::ItemReward()
+{
+    
+}
+
+
+void BattleSystem::PlayerAttack()
+{
+    int damage = player->GetATK();
+    monster->TakeDamage(damage);
+}
+
+
+void BattleSystem::MonsterAttack()
+{
+    int damage = monster->GetATK();
+    player->TakeDamage(damage);
+}
+
+
+void BattleSystem::BattleReward()
+{
+    
+}
+
+
+void BattleSystem::ItemReward()
+{
+    
+}
 
 
 void BattleSystem::ChoiceMenu()
