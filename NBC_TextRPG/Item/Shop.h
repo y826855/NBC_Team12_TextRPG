d9849c9a7b1b01617266cfd8f5b3fa53deef1bdc
songmanok <rjsgh33399@gmail.com>
2026-05-26 12:42:04 +0900

    feat:shop.h추가

---
+++ NBC_TextRPG/Item/Shop.h
@@ -0,0 +1,15 @@
+﻿#pragma once
+
+class Player;
+class Inventory;
+
+class Shop
+{
+public:
+    void OpenShop(Player* player, Inventory* inventory);
+    
+private:
+    void BuyItem(Player* player, Inventory* inventory);
+    
+    void SellItem(Player* player, Inventory* inventory);
+};
