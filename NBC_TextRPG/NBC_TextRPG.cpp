03a98c7b42b8a1e1dbf9d11100efa8db6e21971d
DESKTOP-JV9Q3R0\y8268 <y826855@naver.com>
2026-05-26 22:06:59 +0900

    fix : 메인에서 창을 컨트롤 하게 수정함.  - 로비에서 창을 닫지 않게 수정

--- NBC_TextRPG/NBC_TextRPG.cpp
+++ NBC_TextRPG/NBC_TextRPG.cpp
@@ -1,8 +1,18 @@
-﻿#include "MultiConsole/ConsoleController.h"
-#include "Utility/InputHelper.h"
+﻿#include <iostream>
+
+#include "MultiConsole/ConsoleController.h"
 #include "Lobby.h"
 
 using namespace std;
+
+void PrepareWindow()
+{
+    ConsoleController::GetInstance()->AddConsole(EConsoleTag::Main);
+    ConsoleController::GetInstance()->AddConsole(EConsoleTag::UpperRight);
+    ConsoleController::GetInstance()->AddConsole(EConsoleTag::LowerRight);
+
+    std::cout << endl << endl;
+}
 
 int main(int argc, char* argv[])
 {
@@ -11,12 +21,26 @@ int main(int argc, char* argv[])
         return 0;
     }
 
-    Lobby* lobby = new Lobby();
-
-    lobby->Intro();
-    lobby->ContentLoop();
-
-    delete lobby;
+    /*
+    //창 위치를 지정할때 주석 해제
+    ConsoleController::GetInstance()->AddConsole(EConsoleTag::Main);
+    ConsoleController::GetInstance()->AddConsole(EConsoleTag::UpperRight);
+    ConsoleController::GetInstance()->AddConsole(EConsoleTag::LowerRight);
+    ConsoleController::GetInstance()->AddConsole(EConsoleTag::LargePopup);
+    ConsoleController::GetInstance()->AddConsole(EConsoleTag::SmallPopup);
     
+    // 위치 측정 도구 실행
+    EditConsole editor;
+    editor.StartTracking();
+    */
+
+    PrepareWindow();
+    
+    Lobby lobby;
+    lobby.Intro();
+    lobby.ContentLoop();
+
+    ConsoleController::GetInstance()->CloseAll();
+
     return 0;
 }
