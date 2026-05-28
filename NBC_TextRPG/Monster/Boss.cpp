8e1eebfbf7247aaa9efbf2ab9caa9f090dd89c77
DESKTOP-JV9Q3R0\y8268 <y826855@naver.com>
2026-05-28 12:16:24 +0900

    hotfix : 보스 등장 개행 처리 수정 & 보스 공격 텍스트 수정

--- NBC_TextRPG/Monster/Boss.cpp
+++ NBC_TextRPG/Monster/Boss.cpp
@@ -11,7 +11,7 @@ Boss::Boss(MonsterData monsterdata) : Monster( )
     this->monsterData.exp = monsterdata.exp;
     this->monsterData.gold = monsterdata.gold;
     this->monsterData.dropItem = monsterdata.dropItem;
-    cout << "\n\n*** 경고!! 담당 튜터 팍이 소환됩니다! ***";
+    cout << "\n\n*** 경고!! 담당 튜터 팍이 소환됩니다! ***\n\n";
     AsciiArt();
    this->SetBossStat();
  
@@ -25,7 +25,7 @@ Boss::~Boss()
 void Boss::Attack()
 {
     //monsterData.mHP = monsterData.mHP - GetPlayer()->GetHp(); -> 임시 주석화
-    cout << "사랑의 뺨따귀: 철썩!\n" << endl;
+    cout << "사랑의 뺨따귀: 철썩!!\n" << endl;
     GetPlayer()->TakeDamage(mATK);
 }
 
